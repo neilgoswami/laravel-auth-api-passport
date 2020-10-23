8280a43c780f5622f8a2db089384457ab2a3ce6a
Neil Goswami <neil@viralmd.com>
2020-10-22 21:58:06 -0500

    Multiple git user testing

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -25,7 +25,6 @@ class AuthController extends Controller
 		return response(['user' => $user, 'access_token' => $accessToken]);
 	}
 
-
 	public function login(Request $request)
 	{
 		$loginData = $request->validate([
